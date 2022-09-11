09c383590d5023bb1c534c930d7958a1edf6faab
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-09-11 10:21:44 +0000

    forms x2

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ControlledForm from './components/ControlledForm';
+
+function App() {
+  return (
+    <div className="App">
+      <ControlledForm />
+    </div>
+  );
+}
+
+export default App;
